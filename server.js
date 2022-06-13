e3557f02d351c5724c77d25aceebf815a80b105f
RazorHollow <rob@razorhollow.com>
2022-06-13 18:22:36 -0400

    add views and import express/ejs

---
+++ server.js
@@ -0,0 +1,22 @@
+//import modules
+import express from 'express'
+
+//create express app
+
+const app = express()
+
+//configure the app
+
+app.set('view engine', 'ejs')
+
+//mount routes
+
+app.get('/', function(req,res) {
+  res.render('home')
+})
+
+//tell app to listen on port 3000
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000')
+})
