4df90a61f04a2ce187508012bb9d2f944c5c8032
RazorHollow <rob@razorhollow.com>
2022-06-13 20:42:24 -0400

    add sample data to simulated db

--- data/prospects.js
+++ data/prospects.js
@@ -6,7 +6,32 @@ email: "jdrake@gmail.com",
 phone: "555-867-5309",
 startDate: Date("2022-03-04T09:00:00"),
 about: "Joe likes Indy Car racing and scrabble. His birthday is March 2nd. His favorite color is blue."
-}]
+},
+{
+id: 1003,
+name: "Bill Shooster", 
+email: "bigbill@gmail.com",
+phone: "555-543-8749",
+startDate: Date("2022-02-04T09:00:00"),
+about: "Bill loves the Buffalo Bills. He has a dog named scooter."
+},
+{
+id: 1005,
+name: "Dave Brewster", 
+email: "brewdog@gmail.com",
+phone: "555-887-2309",
+startDate: Date("2022-03-04T09:00:00"),
+about: "Dave is a Leo. His favorite food is strawberry icecream. He makes movies as a hobby"
+},
+{
+id: 1009,
+name: "Buster Thompson", 
+email: "bambam@gmail.com",
+phone: "555-234-5678",
+startDate: Date("2022-03-04T09:00:00"),
+about: "Buster used to be a semi-pro boxer. He likes the beach and warm weather."
+},
+]
 
 export { 
   prospects
